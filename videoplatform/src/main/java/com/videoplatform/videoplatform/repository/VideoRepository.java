11a307c84b7d020d8181489c7d617f78b87ddb53
Rafiatu Ibrahim <irafiatu44@gmail.com>
2024-06-23 12:31:00 +0000

    configured email sevices

--- videoplatform/src/main/java/com/videoplatform/videoplatform/repository/VideoRepository.java
+++ videoplatform/src/main/java/com/videoplatform/videoplatform/repository/VideoRepository.java
@@ -1,4 +1,11 @@
 package com.videoplatform.videoplatform.repository;
 
-public interface VideoRepository {
+import com.videoplatform.videoplatform.entity.Video;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VideoRepository extends JpaRepository<Video, Integer> {
+
+
 }
---
+++ videoplatform/src/main/java/com/videoplatform/videoplatform/service/EmailService.java
@@ -0,0 +1,40 @@
+package com.videoplatform.videoplatform.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSendException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+    private static final Logger LOGGER=Logger.getLogger(EmailService.class.getName());
+
+    @Autowired
+    public EmailService(JavaMailSender mailSender){
+
+        this.mailSender=mailSender;
+
+    }
+    public void SendEmail(String to, String subject, String text){
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(text);
+            mailSender.send(message);
+            LOGGER.info("Email sent successfully to " + to);
+        }
+        catch (MailSendException e){
+            LOGGER.log(Level.SEVERE,"Failed to send email: " +e.getMessage(),e);
+            throw new RuntimeException("Failed to send email: " +e.getMessage(), e);
+
+        }
+    }
+
+}
